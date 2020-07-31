b932c33c84647d3adb2c7ab3a83cd79c8d0402b4
Golden Eagle <joowoon.kang@biola.edu>
2020-07-31 00:25:58 -0700

    login is working now

--- textagram/src/components/Home/NavBar.js
+++ textagram/src/components/Home/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "../styles/app.scss";
 import { Link, NavLink, withRouter } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 import { IoIosPeople, IoIosSearch } from "react-icons/io";
 import { BsFillPersonCheckFill } from "react-icons/bs";
 
@@ -21,12 +22,10 @@ const NavBar = () => {
           <IoIosSearch size="1.5em" style={{ color: "#000" }} />
         </button>
       </form>
-      {localStorage.token ? (
-        <></>
-      ) : (
-        <button className="nav-att">
-          <BsFillPersonCheckFill size="1.5em" />
-        </button>
+      {localStorage.getItem("token") ? null : (
+        <Nav.Link className="nav-att" href="/login">
+          <BsFillPersonCheckFill size="2.0em" />
+        </Nav.Link>
       )}
     </nav>
   );
