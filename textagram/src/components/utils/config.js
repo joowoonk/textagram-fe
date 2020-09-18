28a2c1d5f08dff0bcb598f2151656a6dccbc3015
Your Name <joowoonk@gmail.com>
2020-09-18 10:59:08 -0700

    forgot to put api

--- textagram/src/components/utils/config.js
+++ textagram/src/components/utils/config.js
@@ -5,7 +5,7 @@ const NODE_ENV = process.env.REACT_APP_FORCE_NODE_ENV || process.env.NODE_ENV;
 if (NODE_ENV === "development") {
   baseURL = "http://localhost:3300/api";
 } else if (NODE_ENV === "production") {
-  baseURL = "https://textagram-be.herokuapp.com/";
+  baseURL = "https://textagram-be.herokuapp.com/api";
   //when heroku is deployed for api...
 }
 
